b174c00999e972131dff3e8e768cdeca07fa9adc
matdevv <matdevvyt@gmail.com>
2025-02-02 04:56:31 -0500

    relocation

--- src/client/character/input.client.ts
+++ src/client/character/input.client.ts
@@ -3,10 +3,10 @@ import { UserInputService, ReplicatedStorage } from "@rbxts/services";
 const keyPressEvent = ReplicatedStorage.WaitForChild("KeyPressEvent") as RemoteEvent;
 
 UserInputService.InputBegan.Connect((input, gameProcessed) => {
-	if (gameProcessed) return;
+	if (gameProcessed === true) return;
 
 	if (input.KeyCode === Enum.KeyCode.E) {
 		print("E key pressed! Sending to server...er");
-		keyPressEvent.FireServer("E");
+		keyPressEvent.FireServer();
 	}
 });
