0671a03ec41ad12a734b79082320b5b9f87d2fa4
matdevv <matdevvyt@gmail.com>
2025-02-02 06:32:04 -0500

    commit

--- src/client/character/input.client.ts
+++ src/client/character/input.client.ts
@@ -3,9 +3,9 @@ import { UserInputService, ReplicatedStorage } from "@rbxts/services";
 //const keyPressEvent = ReplicatedStorage.WaitForChild("KeyPressEvent") as RemoteEvent;
 
 UserInputService.InputBegan.Connect((input, gameProcessed) => {
-	if (gameProcessed === true) return;
+	if (gameProcessed) return;
 
 	if (input.KeyCode === Enum.KeyCode.E) {
-		print("pressed by the chairman!");
+		print("input test");
 	}
 });
