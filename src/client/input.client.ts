9100243cd70138e045e3200385217832d06d671a
matdevv <matdevvyt@gmail.com>
2025-02-02 04:09:50 -0500

    test

--- src/client/input.client.ts
+++ src/client/input.client.ts
@@ -2,14 +2,11 @@ import { UserInputService, ReplicatedStorage } from "@rbxts/services";
 
 const keyPressEvent = ReplicatedStorage.WaitForChild("KeyPressEvent") as RemoteEvent;
 
-// Function to detect key presses
-
 UserInputService.InputBegan.Connect((input, gameProcessed) => {
-	if (gameProcessed) return; // Ignore inputs that are already being handled
+	if (gameProcessed) return;
 
 	if (input.KeyCode === Enum.KeyCode.E) {
-		// Change this key to whatever you want
-		print("E key pressed! Sending to server...");
+		print("E key pressed! Sending to server...er");
 		keyPressEvent.FireServer("E");
 	}
 });
